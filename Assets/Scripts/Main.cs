e6f089163ff50d30bb03bf4795290e695bcfa64c
Andrea Klost <anklost@siue.edu>
2025-10-24 20:18:05 -0500

    Enemies are now randomly spawned

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+    private static Main S;
+
+    [Header("Inscribed")]
+    public GameObject[] prefabEnemies; // Array of enemy prefabs
+    public float enemySpawnPerSecond = 0.5f; // Enemies spawned per second
+    public float enemyInsetDefault = 1.5f; // Inset from sides
+
+    private BoundsCheck bndCheck;
+
+    void Awake() {
+        S = this;
+        bndCheck = GetComponent<BoundsCheck>();
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+    
+    public void SpawnEnemy() { 
+        // Pick random enemy prefab
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+        
+        // Position Enemy above the screen at a random x position
+        float enemyInset = enemyInsetDefault;
+        if (go.GetComponent<BoundsCheck>() != null) {
+            enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+        
+        // Set initial position for spawned enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyInset;
+        float xMax = bndCheck.camWidth - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+        
+        // Invoke again
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+}
