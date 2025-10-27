89909839bea265597ba05950f1e744e091e8241f
Andrea Klost <anklost@siue.edu>
2025-10-27 18:15:15 -0500

    Enemy_2 now rotates while changing direction

--- Assets/Scripts/Enemy_2.cs
+++ Assets/Scripts/Enemy_2.cs
@@ -8,11 +8,14 @@ public class Enemy_2 : Enemy {
     [Header("Enemy_2 Inscribed")]
     public float lifeTime = 10;
     [Tooltip("Determines how much the sine wave eases the interpolation")] public float sinEccentricity = 0.6f;
-
+    public AnimationCurve rotCurve;
+    
     [Header("Enemy_2 Private")]
     [SerializeField] private float birthTime;
     [SerializeField] private Vector3 p0, p1;
 
+    private Quaternion baseRotation;
+    
     void Start() {
         // Pick point on left side of the screen
         p0 = Vector3.zero;
@@ -31,6 +34,11 @@ public class Enemy_2 : Enemy {
         }
 
         birthTime = Time.time;
+        
+        // Set up initial rotation
+        transform.position = p0;
+        transform.LookAt(p1, Vector3.back);
+        baseRotation = transform.rotation;
     }
 
     public override void Move() {
@@ -43,6 +51,10 @@ public class Enemy_2 : Enemy {
             Destroy(this.gameObject);
             return;
         }
+        
+        // Use rotCurve to set rotation about Y
+        float shipRot = rotCurve.Evaluate(u) * 360;
+        transform.rotation = baseRotation * Quaternion.Euler(-shipRot, 0, 0);
         
         // Adjust u by adding a U curve based on a Sine wave
         u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
